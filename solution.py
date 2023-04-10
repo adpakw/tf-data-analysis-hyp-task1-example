c36c66cbafb427cd243e162b2ce7329d5d1060eb
adpakw <94380412+adpakw@users.noreply.github.com>
2023-04-11 00:40:38 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,16 @@
 import pandas as pd
 import numpy as np
+import math
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 451783978 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+  p1 = x_success / x_cnt
+  p2 = y_success / y_cnt
+  p = (x_success + y_success) / (x_cnt + y_cnt)
+  z = (p2 - p1) / sqrt(p*(1-p)*(1/x_cnt + 1/y_cnt))
+  z_alpha = norm.ppf(1 - 0.06)
+  return math.fabs(z) > z_alpha
